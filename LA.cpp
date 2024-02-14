bb0cb0dc00d40bc5675e16c4b6322a61bb3021f6
Samantha Rehome <134735450+srehome@users.noreply.github.com>
2024-02-14 15:46:48 -0800

    Created cpp file for LA

---
+++ LA.cpp
@@ -0,0 +1,17 @@
+//create list (set?) for the keywords
+
+
+//create list (set?) for the operators
+
+
+//create list (set?) for the separators
+
+
+//implement FSM for the identifiers
+
+
+//implement FSM for the integers
+
+
+//implement FSM for the real numbers
+
