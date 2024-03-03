90b7802be43aa4f7e7958237e0bee8a20fb36cd2
Samantha Rehome <134735450+srehome@users.noreply.github.com>
2024-03-02 18:24:41 -0800

    changed placement of getchar; moved while from lexer to main; moved comments/whitespace check to main

--- LA.cpp
+++ LA.cpp
@@ -17,7 +17,7 @@ unordered_set<string> keywords{"function", "integer", "boolean", "real", "if", "
 //Function Declarations
 string ID_FSM(char, ifstream&);
 pair<string, string> Int_Real_DFSM(char, ifstream&);
-void lexer(string, vector<pair<string, string>>&);
+pair<string, string> lexer(char,  ifstream&);
 
 //hardcde id DFSM 2D array
 int idDFSM[6][3] = {
@@ -41,72 +41,40 @@ int realDFSM[5][2] = {
 
 //Lexical Analyzer function
 
-void lexer(string filename, vector<pair<string, string>> &tokens) {
-    //Open the file and create a variable to keep track of current character
-    ifstream codefile (filename);
-    char mychar;
+pair<string, string> lexer(char mychar, ifstream &codefile) {
 
-    //while the file is not at the end, parse tokens, identify them, and add them to the vector
-    while( codefile.is_open() && !codefile.eof() ) {
-        //get next character in file
-        mychar = codefile.get();
-
-        if(mychar == '['){  //code to skip comments
-            if(codefile.get() == '*'){  //if next char is '*', it is the start of a comment
-                //find the end of the comment or the end of the file
-                while(!codefile.eof()) {
-                    //check if charcter starts end comment symbol
-                    if(codefile.get() == '*'){
-                        if(!codefile.eof()){            //if still not end of file
-                            if(codefile.get() == ']'){  //check if complete end comment symbol
-                                break;                  //if comment ended, break out of reading comment loop
-                            }
-                        }
-                    }
-                }
-            }
-            else {              //else not start of comment, it's just illegal
-                codefile.unget();       //go back to '['
-                tokens.push_back(make_pair(string(1, mychar), "illegal"));  //add '[' to list of token as illegal
-            }
+    //check if character is in letters string
+    if(letters.find_first_of(tolower(mychar)) != string::npos){
+        string token = ID_FSM(mychar, codefile);    //call id fsm to read in and return full token
+        string lexeme = keywords.find(token) != keywords.end() ? "keyword" : "identifier";  //if found in keywords, lexeme set to keyword, else identifier
+        return make_pair(token, lexeme); //add id/keyowrd to list of tokens
+    }
+    //check if character is in digits string
+    else if(digits.find_first_of(mychar) != string::npos) {
+        return Int_Real_DFSM(mychar, codefile);  //add int/real to list of tokens
+    }
+    //check if character is in separators string
+    else if(separators.find_first_of(mychar) != string::npos) {
+        return make_pair(string(1, mychar), "separator");    //add separator to list of tokens
+    }
+    //check if character is in operators string
+    else if(operators.find_first_of(mychar) != string::npos) {
+        char tempOp = codefile.get(); //Scan next character & store into temporary value
+        //check for == => != <= operators
+        if (((mychar == '=') && (tempOp == '=' || tempOp == '>')) ||
+            ((mychar == '!') && (tempOp == '=')) ||
+            ((mychar == '<') && (tempOp == '='))) {
+                //add concatenated operator to list of tokens
+                return make_pair(string(1, mychar) + string(1, tempOp), "operator");
         }
-        //check if character is in letters string
-        else if(letters.find_first_of(tolower(mychar)) != string::npos){
-            string token = ID_FSM(mychar, codefile);    //call id fsm to read in and return full token
-            string lexeme = keywords.find(token) != keywords.end() ? "keyword" : "identifier";  //if found in keywords, lexeme set to keyword, else identifier
-            tokens.push_back(make_pair(token, lexeme)); //add id/keyowrd to list of tokens
+        else { //add simple operator to list of tokens
+            codefile.unget();
+            return make_pair(string(1, mychar), "operator");
         }
-        //check if character is in digits string
-        else if(digits.find_first_of(mychar) != string::npos) {
-            tokens.push_back(Int_Real_DFSM(mychar, codefile));  //add int/real to list of tokens
-        }
-        //check if character is in separators string
-        else if(separators.find_first_of(mychar) != string::npos) {
-            tokens.push_back(make_pair(string(1, mychar), "separator"));    //add separator to list of tokens
-        }
-        //check if character is in operators string
-        else if(operators.find_first_of(mychar) != string::npos) {
-            char tempOp = codefile.get(); //Scan next character & store into temporary value
-            //check for == => != <= operators
-            if (((mychar == '=') && (tempOp == '=' || tempOp == '>')) ||
-                ((mychar == '!') && (tempOp == '=')) ||
-                ((mychar == '<') && (tempOp == '='))) {
-                    //add concatenated operator to list of tokens
-                    tokens.push_back(make_pair(string(1, mychar) + string(1, tempOp), "operator"));
-            }
-            else { //add simple operator to list of tokens
-                codefile.unget();
-                tokens.push_back(make_pair(string(1, mychar), "operator"));
-            }
-        }
-        //check if character is whitespace
-        else if(isspace(mychar)){
-            //do nothing to skip whitespace
-        }
-        //if character is not recognized above, add to list of tokens as an illegal
-        else {
-            tokens.push_back(make_pair(string(1, mychar), "illegal"));
-        }
+    }
+    //if character is not recognized above, add to list of tokens as an illegal
+    else {
+        return make_pair(string(1, mychar), "illegal");
     }
 }
 
@@ -115,7 +83,7 @@ void lexer(string filename, vector<pair<string, string>> &tokens) {
 
 string ID_FSM(char mychar, ifstream &codefile) {
     int state = 1;
-    string token = string(1, mychar);
+    string token = "";
 
     while (true){
         if(letters.find_first_of(tolower(mychar)) != string::npos){ //if character in letters string
@@ -156,7 +124,7 @@ string ID_FSM(char mychar, ifstream &codefile) {
 pair<string, string> Int_Real_DFSM(char mychar, ifstream& codefile)
 {
     int state = 1;
-    string token = string(1, mychar);
+    string token = "";
 
     while (true) {
     if (digits.find_first_of(mychar) != string::npos) {      //if character is in digits string
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <stdio.h>
 #include <string>
 #include <vector>
 #include "LA.cpp"
@@ -9,33 +10,72 @@ int main () {
     std::string filename;
     std::vector<std::pair<std::string, std::string>> tokenVector;
 
-    std::cout << "Please enter a filename: ";
+    //ask user to provide source code text file
+    std::cout << "Please enter a source code text filename: ";
     std::cin >> filename;
 
-    lexer(filename, tokenVector);
+    //Open the filestream
+    ifstream codefile (filename);
 
+    //if file didn't open, leave function
+    if(!codefile.is_open())
+        return -1;
+
+    //create a variable to keep track of current character
+    char mychar = codefile.get();
+
+    //while the file is not at the end, parse tokens, identify them, and add them to the vector
+    while(!codefile.eof()) {
+        //code to skip comments
+        if(mychar == '['){  
+            if(codefile.get() == '*'){  //if next char is '*', it is the start of a comment
+                //find the end of the comment or the end of the file
+                while(!codefile.eof()) {
+                    //check if charcter starts end comment symbol
+                    if(codefile.get() == '*'){
+                        if(!codefile.eof()){            //if still not end of file
+                            if(codefile.get() == ']'){  //check if complete end comment symbol
+                                break;                  //if comment ended, break out of reading comment loop
+                            }
+                        }
+                    }
+                }
+            }
+            else {                      //else not start of comment, it's just illegal
+                codefile.unget();       //go back to '['
+                std::string token = "illegal";
+                tokenVector.push_back(std::make_pair(std::string(1, mychar), token));  //add '[' to list of tokens as illegal
+            }
+        }
+        //check if character is whitespace
+        else if(isspace(mychar)){
+            //do nothing to skip whitespace
+        }
+        //call lexer and add lexeme, token pair to vector
+        else{
+            tokenVector.push_back(lexer(mychar, codefile));
+        }
+        
+        //get next character
+        mychar = codefile.get();
+    }
+
+    //ask user to provide name of output text file
+    std::cout << "Please enter an output text filename: ";
+    std::cin >> filename;
+
+    //open output text file to write stdout to it
+    freopen(filename.c_str(), "w", stdout );
+
+    std::cout << std::left << setw(15) << "Token" << "Lexeme" << std::endl;
+    std::cout << "---------------------" << std::endl;
+
+    //iterate through the vector and print contents to output file
     for(std::pair<std::string, std::string> token : tokenVector){
         std::cout << std::left << setw(15) << token.second << token.first << std::endl;
     }
+    
+    freopen("CON", "w", stdout);    //fclose (stdout);
 
     return 0;
 }
-/*  Helpful Code  */
-/*
-    isspace(mychar)   --  checks if character is whitespace (space, tab, newline, etc)
-    tolower(mychar)   --  converts char to lower
-    string(1, mychar) --  converts char to string (for adding to vector)
-    Vector:
-    Add elements to the vector
-        myVector.push_back(make_pair(1, 3.14));
-        myVector.push_back(make_pair(2, 2.718));
-    Access and manipulate elements
-        First element of the first pair: myVector[0].first
-        Second element of the first pair: myVector[0].second
-        First element of the second pair: myVector[1].first
-        Second element of the second pair: myVector[1].second 
-    Loop through vector
-        for (const auto& pair : myVector) {
-            cout << pair.first << ": " << pair.second << endl;
-        }
-*/
